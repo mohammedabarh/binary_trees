149d47f56d90f5d0c8088fb372e5755f209a5fdf
mohammedabarh <abmohammed606@gmail.com>
2024-12-05 15:17:42 -0800

    binary_trees

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,19 @@
+/* 7-binary_tree_inorder.c */
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - Traverses a binary tree using in-order
+ * @tree: Pointer to the root node of the tree to traverse
+ * @func: Pointer to a function to call for each node's value
+ *
+ * Description: Performs in-order traversal on a binary tree
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
+}
---
+++ 9-binary_tree_height.c
@@ -0,0 +1,24 @@
+/* 9-binary_tree_height.c */
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure
+ *
+ * Return: Height of the tree, 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height = 0;
+	size_t right_height = 0;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left)
+		left_height = 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		right_height = 1 + binary_tree_height(tree->right);
+
+	return (left_height > right_height ? left_height : right_height);
+}
