ad231e7e260043aef8f038f6129752f9e3680890
mohammedabarh <abmohammed606@gmail.com>
2024-12-05 13:19:03 -0800

    binary_trees

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,9 +2,8 @@
 
 /**
  * binary_tree_node - Creates a binary tree node
- *
  * @parent: Pointer to the parent node
- * @value: Value to put in the new node
+ * @value: Value for the new node
  *
  * Return: Pointer to the new node, or NULL on failure
  */
@@ -17,11 +16,10 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
     {
         return (NULL);
     }
-
     new_node->n = value;
     new_node->parent = parent;
     new_node->left = NULL;
     new_node->right = NULL;
-
+    
     return (new_node);
 }
