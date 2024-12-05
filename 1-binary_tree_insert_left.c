e16a1dbb57b8cacc767f3f30034d3c0f921c77dd
mohammedabarh <abmohammed606@gmail.com>
2024-12-05 13:57:02 -0800

    binary_trees

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,21 +9,21 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *node;
+	binary_tree_t *node;
 
-    if (!parent)
-        return (NULL);
+	if (!parent)
+		return (NULL);
 
-    node = binary_tree_node(parent, value);
-    if (!node)
-        return (NULL);
+	node = binary_tree_node(parent, value);
+	if (!node)
+		return (NULL);
 
-    if (parent->left)
-    {
-        node->left = parent->left;
-        parent->left->parent = node;
-    }
-    parent->left = node;
+	if (parent->left)
+	{
+		node->left = parent->left;
+		parent->left->parent = node;
+	}
+	parent->left = node;
 
-    return (node);
+	return (node);
 }
